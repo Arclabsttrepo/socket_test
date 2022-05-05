70489bfeffa98a18510b85fc4870923a59c0fcc1
arclabstt <96589494+arclabstt@users.noreply.github.com>
2022-05-05 08:59:51 -0400

    neater update to handle multiple clients
    this sorts out the different message senders

--- sentry_mock/watchdog.py
+++ sentry_mock/watchdog.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-# server
-
-from platform import node
+# client
 import socket
+import time
 import threading
-import conversion
 import pdb
-import subprocess
+import conversion
+import select
 
 # define constants:
 HEADER = 64
@@ -15,153 +14,166 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+NODE_NAME = "explore"
 
-# dictionary for topics
-topics = []
-# dictionary for node names
-nodes = {}
+#declare varaibles for each quantity you want to receive
+odometry = []
+zed = []
 
-# lock for sharing variables between threads
+#place them in a list
+variableList=[None]*2 #where 2 is the amount of variables you have
+#variableList=[odometry, zed]
+
+#write their names in the same order as above
+#so that these strings can be compared to the key
+#to match message data with variables
+variableNameList = ["odometry", "zed"]
+
+
+# create client socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# attempt connection
+try:
+    client.connect(ADDR)
+except ConnectionRefusedError:
+    print("[UNABLE TO CONNECT TO WATCHDOG]")
+    print("[QUITTING]")
+    quit()
+
+# lock object needed to manage access to sockets receiving
+# so far only used for receiving as no other thread is sending
+# only the main thread is sending
 lock = threading.Lock()
+'''
+#using lock with context manager:
+with lock:
+    #statements
+# is equivalent to:
+lock.aquire()
+try:
+    #statements
+finally:
+    #statements
+'''
 
-# topics are names for variables that are meant to be updated periodically
-# many nodes can push to them and many nodes can pull from them
-# although it makes more sense for one node to push to them while one or many
-# nodes pull from them
-# the value stored in a topic is the last one updated by the node that generates it
-# nodes are individual scripts
 
-# create server socket object
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-
-
-def Send_string_to_client(sent_from_node, conn, msg):
-    
-    msg_json_string = conversion.Conversion_To_Json(sent_from_node,msg)
-    message_utf = msg_json_string.encode(FORMAT)
+def Send_string_to_watchdog(destination,msg):
+    message_string = conversion.Conversion_To_Json(destination,msg)
+    message_utf = message_string.encode(FORMAT)
     msg_length = len(message_utf)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
-    conn.send(send_length)
-    conn.send(message_utf)
-    print("[SENDING TO CLIENT] " + str(msg))
+    client.send(send_length)
+    client.send(message_utf)
+    print("[SENDING TO WATCHDOG] " + str(msg))
 
 
-def Receive_string_from_client(conn):
-    
-    received_msg_length = conn.recv(HEADER).decode(FORMAT)
+def Receive_string_from_watchdog():
+
+    received_msg_length = client.recv(HEADER).decode(FORMAT)
     # in the event that the socket sends something blank:
-   
+
     if received_msg_length:
         received_msg_length = int(received_msg_length)
-        received_msg = conn.recv(received_msg_length).decode(FORMAT)
-        print("[CLIENT SAID] " + str(received_msg))
+        received_msg = client.recv(received_msg_length).decode(FORMAT)
+        #print("[WATCHDOG SAID] " + str(received_msg))
         received_msg_dict=conversion.Json_To_Dict(received_msg)
         return received_msg_dict
     else:
         return "other connection closed"
 
 
-def Push(conn, msg, node_name):
+
+# outgoing message handler:
+def Push_to_node(node_name, msg2):
+    # send command to server plus node to send message to
+
+    Send_string_to_watchdog(node_name, msg2)
+    '''
     try:
-        # lock so no other threads can access these sockets
-        lock.acquire()
-        
-        #store the name of the node being sent to and the message
-        node_to_send_msg_to = msg['key']
-        msg_to_send_to_node = msg['msg']
-        
+        # pdb.set_trace()
+        #Send_string_to_watchdog("push:"+msg2)
+        Send_string_to_watchdog("push", node_name, msg2)
+        #temp = Receive_string_from_watchdog()
+        #if temp == "node_found_push_the_data":
 
-        #This also checks that node_to_send_to exists in the dictionary. That means that the node is alive and connected. Else KeyError is raised.
-        node_to_send_to_socket_object=nodes[node_to_send_msg_to]
-        Send_string_to_client(node_name, node_to_send_to_socket_object, msg_to_send_to_node)
+        #    Send_string_to_watchdog(msg2)
+            # print(Receive_string_from_watchdog())
+        #else:
+        #    print(f"[{NODE_NAME}] says: Pushing to {node_name} failed!")
+    except AttributeError:
+        print("Incorrect data type sent, recheck")
+        quit()
+    '''
 
-        # unlock so other threads can access these sockets
-        lock.release()
-    except KeyError:
-        #if node not registered with watchdog, this error is thrown
-        # unlock so other threads can access these sockets
-        lock.release()
-        print(str(node_to_send_msg_to)+" not connected")
-
-        '''
-        FOR FUTURE:
-         if node not found in dictionary
-         print("Node not found in registered nodes list!")
-         so that node that tried to push can take some action
-         knowing that no data was sent to the requested node
-         #Send_string_to_client(conn, "Node not found in registered nodes list!")
-        '''
-
-
-
-
-def Handle_client(conn, addr, node_name):
-    print(f"[NEW CONNECTION] {addr}  {node_name} connected.")
-    connected = True
-
-    try:
-        while connected:
-            msg = Receive_string_from_client(conn)
-            if msg['key'] == "watchdog" and msg['msg'] == DISCONNECT_MESSAGE:
-                print(f"{node_name} has disconnected.")
-                nodes.pop(node_name)
-                connected = False
-
-            #print(f"[{node_name}] says: {msg}")
-
-            if msg['key'] != "watchdog":
-                Push(conn, msg, node_name)
-
-
-        conn.close()
-        print(f"[CLOSING THREAD]")
-    except ConnectionResetError:
-        print(f"Connection to {node_name} lost!")
-
-
-def Start_server():
-    server.listen()
-   
-    #subprocess.call(['gnome-terminal', '-e', 'python3 client1.py'])
-    #subprocess.call(['gnome-terminal', '-e', 'python3 client2.py'])
-
-    print(f"[LISTENING] Server is listening on {SERVER}")
+def Incoming_messages_handler(variableNameList, variableList, client):
     while True:
-        # wait for new connetion from client
-        conn, addr = server.accept()
-
-        temp_return = Receive_string_from_client(conn)
+        
+        received_message_dict = Receive_string_from_watchdog()
         
 
-        
-        # register the node name in the node dictionary
-        if temp_return and temp_return['key'] == "watchdog":
-            node_name = temp_return['msg']
-            # key=node_name
-            # value=conn
-
-            #check if node already exists:
-            lock.acquire()
-            if node_name in nodes:
-                print(f"{node_name} already registered! Closing connection!")
-                conn.close()
-                lock.release()
-            else:
-                nodes.update({node_name: conn})
-                lock.release()
-                # create a new thread to handle the new connection
-                thread = threading.Thread(target=Handle_client, args=(conn, addr, node_name))
-                thread.start()
-
-                print(f"[ACTIVE CONNETIONS] {threading.active_count() - 1}")
-        else:
-            print("[NODE DID NOT REGISTER] Awaiting new connections.")
+        #look for names of variabes in received message to store its latest value
+        iterator = 0
+        for variableNameIterator in variableNameList:
+            #if variable found in the received message 
+            if received_message_dict['key']==variableNameIterator:
+                #store it in the corresponding variable
+                with lock:
+                    variableList[iterator] = received_message_dict
+            iterator = iterator + 1
 
 
-print("[STARTING SERVER]")
+        #last working code:
+'''        #look for names of variabes in received msg to store its value
+        for variableIndex in range(len(variableNameListTemp)):
+            #if variable found in the received message
+            
+            if received_message_dict['key']==variableNameListTemp[variableIndex]:
+                #store it in the corresponding variable
+                with lock:
+                    variableListTemp[variableIndex]=received_message_dict['msg']
+                    #print(received_message_dict['msg'])
+                #print(str(variableList[variable_index]))'''
+
+
+def Main():
+    # run main code here
+    # process robotic algorthm here
+    # compute stuff here
+
+    while True:
+        # update variables at the start of every loop
+
+        with lock:
+            #same order in decleration
+            odometry=variableList[0]
+            zed=variableList[1]
+        print(f"odometry: {odometry}")
+        print(f"zed: {zed}")
+
+        #last working 
+        '''with lock:
+            for variableIndex in range(len(variableList)):
+                variableList[variableIndex] = variableListTemp[variableIndex]
+    
+        for variableIndex in range(len(variableList)):
+            print(variableList[variableIndex])
+            print("test")'''
+
+ 
+        time.sleep(3)
+
+
+
+
+
+
 try:
-    Start_server()
+    Send_string_to_watchdog("watchdog",NODE_NAME)
+    thread = threading.Thread(target=Incoming_messages_handler, args=(variableNameList, variableList, client))
+    thread.start()
+    Main()
 except KeyboardInterrupt:
-    print("[SHUTTING DOWN SERVER]")
+    Send_string_to_watchdog("watchdog", DISCONNECT_MESSAGE)
+
+Send_string_to_watchdog("watchdog", DISCONNECT_MESSAGE)
